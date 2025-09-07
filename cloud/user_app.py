eb14ea413b47e9f2bc543c400f29d0e173dd1a85
VM-Francesco-Barcherini <f.barcherini@studenti.unipi.it>
2025-09-07 23:30:20 +0200

    ⚡️ Removed relays from user_app

--- cloud/user_app.py
+++ cloud/user_app.py
@@ -139,12 +139,10 @@ def menu():
             print("1. Set HVAC status")
             print("2. Set mode (normal|green)")
             print("3. Set target temperature")
-            print("4. Set relays state")
-            print("5. Set antidust state")
-            print("6. Set relay settings [CRUD]")
-            print("7. Set HVAC settings [CRUD]")
-            print("8. Dump system state [CRUD]")
-            print("9. Exit")
+            print("4. Set antidust state")
+            print("5. Set HVAC settings [CRUD]")
+            print("6. Dump system state [CRUD]")
+            print("7. Exit")
         stop_get_all = False
         choice = input("> ")
 
@@ -177,33 +175,13 @@ def menu():
         elif choice == "4":
             stop_get_all = True
             try:
-                r_sp = int(input("Relay SP (0|1|2): "))
-                r_h = int(input("Relay H (0|1|2): "))
-                payload = {"n": "relay", "r_sp": r_sp, "r_h": r_h, "p_sp": -1, "p_h": -1}
-                send_post("relay", payload)
-            except ValueError:
-                print("Invalid input.")
-        elif choice == "5":
-            stop_get_all = True
-            try:
                 v = input("antiDust value (on|off): ")
                 if v not in ["on", "off"]:
                     raise ValueError("Invalid input")
                 send_post("antiDust", {"n": "antiDust", "v": v})
             except ValueError:
                 print("Invalid input.")
-        elif choice == "6":
-            stop_get_all = True
-            try:
-                r_sp = int(input("Relay SP (0|1|2): "))
-                r_h = int(input("Relay H (0|1|2): "))
-                p_sp = float(input("Power SP: "))
-                p_h = float(input("Power H: "))
-                payload = {"n": "relay", "r_sp": r_sp, "r_h": r_h, "p_sp": p_sp, "p_h": p_h}
-                send_post("relay", payload)
-            except ValueError:
-                print("Invalid input.")
-        elif choice == "7":
+        elif choice == "5":
             stop_get_all = True
             try:
                 pw = float(input("Consumed power: "))
@@ -214,7 +192,7 @@ def menu():
                 send_post("settings", payload)
             except ValueError:
                 print("Invalid input.")
-        elif choice == "8":
+        elif choice == "6":
             stop_get_all = True
             try:
                 r = requests.get(f"{conf.HTTP_SERVER}/all")
@@ -222,7 +200,7 @@ def menu():
                 print(json.dumps(r.json(), indent=2))
             except Exception as e:
                 print(f"[ERROR] Could not GET /all: {e}")
-        elif choice == "9":
+        elif choice == "7":
             stop_get_all = True
             print("Exiting...")
             break
